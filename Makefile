19aaa2cad102f1884de9ba00d93ad306d29190bf
BalakeKarbon <blakekarbon@gmail.com>
2025-10-14 03:53:57 +0000

    Modified makefile for automatic example flags and other tidying, added automatic COBOL to C string conversion.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SRC_DIR := ./src
 BUILD_DIR := ./build
 DEBUG_DIR := ./example
+EXAMPLE_BASE_FLAGS = $(shell ctags -x --c-kinds=f $(SRC_DIR)/cobdom.c | awk '{printf "-K %s ", $$1}' | sed 's/ $$//') 
 
 all: $(BUILD_DIR)/lib/libcobdom.a
 
@@ -10,18 +11,24 @@ $(BUILD_DIR):
 $(BUILD_DIR)/lib: $(BUILD_DIR)
 	mkdir -p $(BUILD_DIR)/lib
 
-$(BUILD_DIR)/lib/libcobdom.a: $(BUILD_DIR)/cobdom.o | $(BUILD_DIR)/lib
+#$(BUILD_DIR)/lib/libcobdom.a: $(BUILD_DIR)/cobdom.o $(BUILD_DIR)/cobdom-cob.o $(BUILD_DIR)/lib
+$(BUILD_DIR)/lib/libcobdom.a: $(BUILD_DIR)/cobdom.o $(BUILD_DIR)/lib
 	emar rcs $@ $<
+	#emar rcs $@ $< $(BUILD_DIR)/cobdom-cob.o
 	#emar rcs $@ $< $(BUILD_DIR)/bridge.o
 
 $(BUILD_DIR)/cobdom.o: $(SRC_DIR)/cobdom.c | $(BUILD_DIR)
 	emcc -c $< -o $@
 
+#$(BUILD_DIR)/cobdom-cob.o: $(BUILD_DIR)/cobdom-cob.c | $(BUILD_DIR)
+#	emcc -c $< -o $@
+
 #$(BUILD_DIR)/bridge.o: $(SRC_DIR)/bridge.c
 #	emcc -c $< -o $@
 
-#$(BUILD_DIR)/cobdom.c: $(SRC_DIR)/cobdom.cob | $(BUILD_DIR)
-#	(cd $(SRC_DIR) && cobc -C -o $(abspath $@) $(notdir $<) -K emscripten_run_script_int -K return_string)
+#$(BUILD_DIR)/cobdom-cob.c: $(SRC_DIR)/cobdom.cob | $(BUILD_DIR)
+#	(cd $(SRC_DIR) && cobc -C -o $(abspath $@) $(notdir $<))
+	#(cd $(SRC_DIR) && cobc -C -o $(abspath $@) $(notdir $<) -K emscripten_run_script_int -K return_string)
 	#find $(BUILD_DIR) -type f -name '*.c' -exec sed -i '/module->module_cancel\.funcptr =/ s/^/\/\//' {} +
 	#cobc -C $< -o $@
 
@@ -29,12 +36,13 @@ $(BUILD_DIR)/example: $(BUILD_DIR)
 	mkdir -p $(BUILD_DIR)/example
 
 $(BUILD_DIR)/example/example.c: all | $(BUILD_DIR)/example 
-	cobc -C -o $@ ./example/example.cob -K cobdom_get_cookie -K cobdom_create_element -K cobdom_inner_html -K cobdom_append_child -K cobdom_remove_child -K cobdom_add_event_listener -K CHECK-COOKIE
+	cobc -C -o $@ ./example/example.cob $(EXAMPLE_BASE_FLAGS) -K CHECK-COOKIE
+	#cobc -C -o $@ ./example/example.cob -K cobdom_get_cookie -K cobdom_create_element -K cobdom_inner_html -K cobdom_append_child -K cobdom_remove_child -K cobdom_add_event_listener -K cobdom_norm -K CHECK-COOKIE
 #	cobc -C -o $@ ./example/host.cob -K COBDOM-VERSION -K COBDOM-CREATE-ELEMENT -K COBDOM-APPEND-CHILD -K COBDOM-REMOVE-CHILD -K COBDOM-INNER-HTML -K COBDOM-ADD-EVENT-LISTENER -K COBDOM-CLASS-NAME -K COBDOM-STYLE -K COBDOM-SET-COOKIE -K COBDOM-GET-COOKIE -K EXAMPLE -K TESTFUNC
 	#find $(BUILD_DIR)/example -type f -name '*.c' -exec sed -i '/module->module_cancel\.funcptr =/ s/^/\/\//' {} +
 
 $(BUILD_DIR)/example/example.js: $(BUILD_DIR)/example/example.c
-	emcc -o $@ $< $(BUILD_DIR)/lib/libcobdom.a -lgmp -lcob -s EXPORTED_FUNCTIONS=_cob_init,_EXAMPLE,_CALLBACK -s EXPORTED_RUNTIME_METHODS=ccall,cwrap
+	emcc -o $@ $< $(BUILD_DIR)/lib/libcobdom.a -lgmp -lcob -s EXPORTED_FUNCTIONS=_cob_init,_EXAMPLE -s EXPORTED_RUNTIME_METHODS=ccall,cwrap
 	#-s STANDALONE_WASM=1
 
 example: $(BUILD_DIR)/example/example.js 
--- src/cobdom.c
+++ src/cobdom.c
@@ -1,6 +1,36 @@
 #include <emscripten.h>
 #include <stdint.h>
 
+char* cobdom_string(char* cobol_string) {
+	// Find the length manually
+	int len = 0;
+	while (cobol_string[len] != '\0') {
+		len++;
+	}
+	
+	// Move backwards over spaces
+	while (len > 0 && cobol_string[len - 1] == ' ') {
+		len--;
+	}
+	
+	// Null-terminate at the new length
+	cobol_string[len] = '\0';
+}
+EM_JS(int, cd_test_string, (int my_string), {
+	try {
+		let myString = UTF8ToString(my_string);
+		console.log(myString + "-end.");
+		return 1;
+	} catch (e) {
+		console.log('CobDOMinate Error:');
+		console.log('  Test string: ' + e);
+		return -1;
+	}
+});
+int cobdom_test_string(const char* my_string) {
+	cobdom_string(my_string);
+	return cd_test_string((intptr_t)my_string);
+}
 EM_JS(int, cd_create_element, (int variable_name,int element_type), {
 	try {
 		let variableName = UTF8ToString(variable_name);
@@ -15,6 +45,8 @@ EM_JS(int, cd_create_element, (int variable_name,int element_type), {
 	}
 });
 int cobdom_create_element(const char *variable_name,const char *element_type) {
+	cobdom_string(variable_name);
+	cobdom_string(element_type);
 	return cd_create_element((intptr_t)variable_name,(intptr_t)element_type);
 }
 EM_JS(int, cd_append_child, (int variable_name,int parent_name), {
@@ -35,6 +67,8 @@ EM_JS(int, cd_append_child, (int variable_name,int parent_name), {
 	}
 });
 int cobdom_append_child(const char *variable_name, const char *parent_name) {
+	cobdom_string(variable_name);
+	cobdom_string(parent_name);
 	return cd_append_child((intptr_t)variable_name,(intptr_t)parent_name);
 }
 EM_JS(int, cd_remove_child, (int variable_name,int parent_name), {
@@ -54,6 +88,8 @@ EM_JS(int, cd_remove_child, (int variable_name,int parent_name), {
 	}
 });
 int cobdom_remove_child(const char *variable_name, const char *parent_name) {
+	cobdom_string(variable_name);
+	cobdom_string(parent_name);
 	return cd_remove_child((intptr_t)variable_name,(intptr_t)parent_name);
 }
 EM_JS(int, cd_inner_html, (int variable_name,int html_content), {
@@ -69,6 +105,8 @@ EM_JS(int, cd_inner_html, (int variable_name,int html_content), {
 	}
 });
 int cobdom_inner_html(const char *variable_name, const char *html_content) {
+	cobdom_string(variable_name);
+	cobdom_string(html_content);
 	return cd_inner_html((intptr_t)variable_name,(intptr_t)html_content);
 }
 EM_JS(int, cd_add_event_listener, (int variable_name,int event_type,int cobol_func), {
@@ -91,6 +129,9 @@ EM_JS(int, cd_add_event_listener, (int variable_name,int event_type,int cobol_fu
 	}
 });
 int cobdom_add_event_listener(const char *variable_name, const char *event_type, const char *cobol_func) {
+	cobdom_string(variable_name);
+	cobdom_string(event_type);
+	cobdom_string(cobol_func);
 	return cd_add_event_listener((intptr_t)variable_name,(intptr_t)event_type,(intptr_t)cobol_func);
 }
 EM_JS(int, cd_remove_event_listener, (int variable_name,int event_type), {
@@ -113,7 +154,9 @@ EM_JS(int, cd_remove_event_listener, (int variable_name,int event_type), {
 		return -1;
 	}
 });
-int cobdom_remove_event_listener(const char *variable_name, const char *event_type) { //JS or function?
+int cobdom_remove_event_listener(const char *variable_name, const char *event_type) {
+	cobdom_string(variable_name);
+	cobdom_string(event_type);
 	return cd_remove_event_listener((intptr_t)variable_name,(intptr_t)event_type);
 }
 
@@ -130,6 +173,8 @@ EM_JS(int, cd_set_class, (int variable_name,int class_name), {
 	}
 });
 int cobdom_set_class(const char *variable_name, const char *class_name) {
+	cobdom_string(variable_name);
+	cobdom_string(class_name);
 	return cd_set_class((intptr_t)variable_name,(intptr_t)class_name);
 }
 EM_JS(int, cd_style, (int variable_name,int style_key,int style_value), {
@@ -146,6 +191,9 @@ EM_JS(int, cd_style, (int variable_name,int style_key,int style_value), {
 	}
 });
 int cobdom_style(const char *variable_name, const char *style_key, const char *style_value) { 
+	cobdom_string(variable_name);
+	cobdom_string(style_key);
+	cobdom_string(style_value);
 	return cd_style((intptr_t)variable_name,(intptr_t)style_key,(intptr_t)style_value);
 }
 EM_JS(int, cd_class_style, (int class_name,int style_key,int style_value), {
@@ -163,7 +211,10 @@ EM_JS(int, cd_class_style, (int class_name,int style_key,int style_value), {
 		return -1;
 	}
 });
-int cobdom_class_style(const char *class_name, const char *style_key, const char *style_value) { 
+int cobdom_class_style(const char *class_name, const char *style_key, const char *style_value) {
+	cobdom_string(class_name);
+	cobdom_string(style_key);
+	cobdom_string(style_value);
 	return cd_class_style((intptr_t)class_name,(intptr_t)style_key,(intptr_t)style_value);
 }
 EM_JS(int, cd_set_cookie, (int data,int cookie_name), {
@@ -179,6 +230,8 @@ EM_JS(int, cd_set_cookie, (int data,int cookie_name), {
 	}
 });
 int cobdom_set_cookie(const char *data, const char *cookie_name) {
+	cobdom_string(data);
+	cobdom_string(cookie_name);
 	return cd_set_cookie((intptr_t)data,(intptr_t)cookie_name);
 }
 EM_JS(int, cd_get_cookie, (int data,int cookie_name), {
@@ -196,5 +249,7 @@ EM_JS(int, cd_get_cookie, (int data,int cookie_name), {
 	}
 });
 int cobdom_get_cookie(char *data, const char *cookie_name) {
+	cobdom_string(data);
+	cobdom_string(cookie_name);
 	return cd_get_cookie((intptr_t)data,(intptr_t)cookie_name);
 }
